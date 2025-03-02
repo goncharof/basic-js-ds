6359e16d8480615496cd8eb882d71f8c67109d8f
yolo <igoncharov@flightpath3d.com>
2025-03-02 14:26:19 +0000

    feat: Implement simple binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,107 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
 
+  #root = null;
+
+  #add(node, data) {
+    if (!node) {
+      return new Node(data);
+    }
+
+    if (data < node.data) {
+      node.left = this.#add(node.left, data);
+    } else {
+      node.right = this.#add(node.right, data);
+    }
+
+    return node;
+  }
+
+  #find(node, data) {
+    if (!node) {
+      return null;
+    }
+
+    if (data === node.data) {
+      return node;
+    } else if (data < node.data) {
+      return this.#find(node.left, data);
+    } else {
+      return this.#find(node.right, data);
+    }
+  }
+
+  #remove(node, data) {
+    if (!node) {
+      return null;
+    }
+
+    if (data < node.data) {
+      node.left = this.#remove(node.left, data);
+    } else if (data > node.data) {
+      node.right = this.#remove(node.right, data);
+    } else {
+      if (!node.left && !node.right) {
+        return null;
+      } else if (!node.left) {
+        return node.right;
+      } else if (!node.right) {
+        return node.left;
+      }
+      const minNode = this.#findMin(node.right);
+      node.data = minNode.data;
+      node.right = this.#remove(node.right, minNode.data);
+    }
+
+    return node;
+  }
+
+  #findMin(node) {
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  #findMax(node) {
+    while (node.right) {
+      node = node.right;
+    }
+    return node;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.#root = this.#add(this.#root, data);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return !!this.#find(this.#root, data);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return this.#find(this.#root, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.#root = this.#remove(this.#root, data);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#findMin(this.#root).data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#findMax(this.#root).data;
   }
 }
 
